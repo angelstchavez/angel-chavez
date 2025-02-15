a7cdfc99efdeb0fff4e5126582dba4bee66b8695
angelstchavez <angelstchavez@gmail.com>
2025-02-15 11:47:58 -0500

    feat: :sparkles: add new projects page and update image

---
+++ app/projects/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/sections/header";
+
+const PorjectsPage = () => {
+  return (
+    <div className="max-w-3xl mx-auto px-4 py-12">
+      <Header />
+    </div>
+  );
+};
+
+export default PorjectsPage;
